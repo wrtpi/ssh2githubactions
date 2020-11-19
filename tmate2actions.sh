04ecb0320836a8c9e3f994e1c0afbdf70fd51445
wrtpi <71971470+wrtpi@users.noreply.github.com>
2020-11-19 23:57:21 +0800

    1
    1

--- tmate2actions.sh
+++ tmate2actions.sh
@@ -48,7 +48,7 @@ TMATE_WEB=$(tmate -S ${TMATE_SOCK} display -p '#{tmate_web}')
 MSG="
 * 🍎 GitHub Actions 😊 tmate session info: 🍉 *
 
-✈️====WRT==== `date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"` ====✈️
+✈️=======WRT======⏰ `date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"` ⏱======✈️
 
 ✳️  *CLI:*   \`${TMATE_SSH}\`
 
